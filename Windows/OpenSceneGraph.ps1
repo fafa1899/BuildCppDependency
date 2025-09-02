87e522ce3c786c82b4688f1be826b201b2c127ce
fafa1899 <holybell44@126.com>
2025-09-02 19:57:28 +0800

    [M]支持构建OSG

--- Windows/OpenSceneGraph.ps1
+++ Windows/OpenSceneGraph.ps1
@@ -1,67 +1,53 @@
-param(   
-    [string]$SourceAddress = "https://github.com/openscenegraph/OpenSceneGraph/archive/refs/tags/OpenSceneGraph-3.6.5.zip",
-    [string]$SourceZipPath = "../Source/OpenSceneGraph-3.6.5.zip",
-    [string]$SourceLocalPath = "./OpenSceneGraph-OpenSceneGraph-3.6.5",
+# OpenSceneGraph.ps1
+param(    
+    [string]$Name = "OpenSceneGraph-3.6.5",
+    [string]$SourceDir = "../Source",
     [string]$Generator,
-    [string]$MSBuild,
-    [string]$InstallDir,
-    [string]$SymbolDir   
+    [string]$InstallDir,  
+    [string]$SymbolDir,  
+    [bool]$Force = $false,        # 是否强制重新构建
+    [bool]$Cleanup = $true        # 是否在构建完成后删除源码和构建目录
 )
 
-# 检查目标文件是否存在，以判断是否安装
-$DstFilePath = "$InstallDir/bin/osg161-osg.dll"
-if (Test-Path $DstFilePath) {
-    Write-Output "The current library has been installed."
-    exit 1
-} 
+# 目标文件
+$DllPath = "$InstallDir/bin/osg161-osg.dll"
 
-# 创建所有依赖库的容器：FBX、GDAL、CURL
-. "./BuildRequired.ps1"
-$Librarys = @("zlib", "libpng", "libjpeg", "libtiff", "giflib", "freetype") 
-BuildRequired -Librarys $Librarys
+# 依赖库数组(FBX、GDAL、CURL)
+$Librarys = @("zlib", "libpng", "libjpeg", "libtiff", "giflib", "freetype", "curl") 
 
-. "./DownloadAndUnzip.ps1"
-DownloadAndUnzip -SourceLocalPath $SourceLocalPath -SourceZipPath $SourceZipPath -SourceAddress $SourceAddress
+# 符号库文件
+$PdbFiles = @() 
 
-# 清除旧的构建目录
-$BuildDir = $SourceLocalPath + "/build"  
-if (Test-Path $BuildDir) {
-    Remove-Item -Path $BuildDir -Recurse -Force
+# 额外构建参数
+$CMakeCacheVariables = @{ 
+    CURL_LIBRARY="$InstallDir/lib/libcurl_imp.lib"
+    CURL_IS_STATIC = "OFF"
+    GIFLIB_LIBRARY = "$InstallDir/lib/giflib.lib"    
+    BUILD_OSG_APPLICATIONS = "ON"
+    BUILD_OSG_EXAMPLES = "OFF"
+    CMAKE_RELWITHDEBINFO_POSTFIX = ""  
 }
-New-Item -ItemType Directory -Path $BuildDir
 
-# 转到构建目录
-Push-Location $BuildDir
+. ./build-common.ps1 -Name $Name `
+    -SourceDir $SourceDir `
+    -InstallDir $InstallDir `
+    -SymbolDir $SymbolDir `
+    -Generator $Generator `
+    -TargetDll $DllPath `
+    -PdbFiles $PdbFiles `
+    -CMakeCacheVariables $CMakeCacheVariables `
+    -MultiConfig $false `
+    -Force $Force `
+    -Cleanup $Cleanup `
+    -Librarys $Librarys
 
-try {
-    # 配置CMake      
-    cmake .. -G "$Generator" -A x64 `
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo `
-        -DCMAKE_PREFIX_PATH="$InstallDir" `
-        -DCMAKE_INSTALL_PREFIX="$InstallDir" `
-        -DGIFLIB_LIBRARY="$InstallDir/lib/giflib.lib" `
-        -DBUILD_OSG_APPLICATIONS=ON `
-        -DBUILD_OSG_EXAMPLES=OFF `
-        -DCMAKE_RELWITHDEBINFO_POSTFIX=""
-        #-DBUILD_OSG_DEPRECATED_SERIALIZERS=OFF `
 
-    # 构建阶段，指定构建类型
-    cmake --build . --config RelWithDebInfo -- /m:8
+# 获取源目录下的所有 .pdb 文件
+$pdbFiles = Get-ChildItem -Path "$InstallDir/bin" -Filter *.pdb -Recurse
 
-    # 安装阶段，指定构建类型和安装目标
-    cmake --build . --config RelWithDebInfo --target install
-
-    # 获取源目录下的所有 .pdb 文件
-    $pdbFiles = Get-ChildItem -Path "$InstallDir/bin" -Filter *.pdb -Recurse
-
-    # 移动每个 .pdb 文件到目标目录
-    foreach ($file in $pdbFiles) {
-        $destinationPath = Join-Path -Path $SymbolDir -ChildPath $file.Name
-        Move-Item -Path $file.FullName -Destination $destinationPath -Force
-        Write-Output "Moved: $($file.FullName) -> $destinationPath"
-    }        
-}
-finally {
-    # 返回原始工作目录
-    Pop-Location
-}
+# 移动每个 .pdb 文件到目标目录
+foreach ($file in $pdbFiles) {
+    $destinationPath = Join-Path -Path $SymbolDir -ChildPath $file.Name
+    Move-Item -Path $file.FullName -Destination $destinationPath -Force
+    Write-Output "Moved: $($file.FullName) -> $destinationPath"
+}        
