138729da79377c415129ca1c1b4e65e51fe543cc
fafa1899 <holybell44@126.com>
2025-09-10 17:23:12 +0800

    [M]优化cpp-httplib.ps1

--- Windows/cpp-httplib.ps1
+++ Windows/cpp-httplib.ps1
@@ -1,28 +1,22 @@
+# libtiff.ps1
 param(    
     [string]$Name = "cpp-httplib-0.25.0",
     [string]$SourceDir = "../Source",
     [string]$Generator,
-    [string]$MSBuild,
     [string]$InstallDir,  
-    [string]$SymbolDir 
+    [string]$SymbolDir,  
+    [bool]$Force = $false,        # 是否强制重新构建
+    [bool]$Cleanup = $true        # 是否在构建完成后删除源码和构建目录
 )
 
-# 根据 $Name 动态构建路径
-$zipFilePath = Join-Path -Path $SourceDir -ChildPath "$Name.zip"
-$SourcePath = Join-Path -Path $SourceDir -ChildPath $Name
-$BuildDir = Join-Path -Path "." -ChildPath $Name
+# 目标文件
+$DllPath = "$InstallDir/include/httplib.h"
 
-# 解压ZIP文件到指定目录
-if (!(Test-Path $SourcePath)) {
-    Expand-Archive -LiteralPath $zipFilePath -DestinationPath $SourceDir -Force
-}
+# 依赖库数组
+$Librarys = @()  
 
-# 检查目标文件是否存在，以判断是否安装
-$DstFilePath = "$InstallDir/include/httplib.h"
-if (Test-Path $DstFilePath) {
-    Write-Output "The current library has been installed."
-    exit 1
-} 
+# 符号库文件
+$PdbFiles = @() 
 
 # 额外构建参数
 $CMakeCacheVariables = @{
@@ -30,12 +24,16 @@ $CMakeCacheVariables = @{
     HTTPLIB_USE_ZSTD_IF_AVAILABLE = "OFF"
 }
 
-# 调用通用构建脚本
-. ./cmake-build.ps1 -SourceLocalPath $SourcePath `
-    -BuildDir $BuildDir `
-    -Generator $Generator `
+. ./build-common.ps1 -Name $Name `
+    -SourceDir $SourceDir `
     -InstallDir $InstallDir `
     -SymbolDir $SymbolDir `
+    -Generator $Generator `
+    -TargetDll $DllPath `
     -PdbFiles $PdbFiles `
     -CMakeCacheVariables $CMakeCacheVariables `
-    -MultiConfig $false     
+    -MultiConfig $false `
+    -Force $Force `
+    -Cleanup $Cleanup `
+    -Librarys $Librarys
+    
