12103d4cbceabacf5b465a6efb55a2f887e646cc
fafa1899 <holybell44@126.com>
2026-04-07 16:48:41 +0800

    [A]新构建脚本

---
+++ Windows/cgal.ps1
@@ -0,0 +1,35 @@
+﻿# cgal.ps1
+param(    
+    [string]$Name = "cgal-5.2.4",
+    [string]$SourceDir = "../Source",
+    [string]$Generator,
+    [string]$InstallDir,  
+    [string]$SymbolDir,  
+    [bool]$Force = $false,        # 是否强制重新构建
+    [bool]$Cleanup = $true        # 是否在构建完成后删除源码和构建目录
+)
+
+# 目标文件
+$DllPath = "$InstallDir/include/CGAL/version.h"
+
+# 依赖库数组
+$Librarys = @("gmp","mpfr")  
+
+# 符号库文件
+$PdbFiles = @()
+
+# 额外构建参数
+$CMakeCacheVariables = @{}
+
+. ./build-common.ps1 -Name $Name `
+    -SourceDir $SourceDir `
+    -InstallDir $InstallDir `
+    -SymbolDir $SymbolDir `
+    -Generator $Generator `
+    -TargetDll $DllPath `
+    -PdbFiles $PdbFiles `
+    -CMakeCacheVariables $CMakeCacheVariables `
+    -MultiConfig $true `
+    -Force $Force `
+    -Cleanup $Cleanup `
+    -Librarys $Librarys
---
+++ Windows/lerc.ps1
@@ -0,0 +1,37 @@
+﻿# lerc.ps1
+param(    
+    [string]$Name = "lerc-4.1.0",
+    [string]$SourceDir = "../Source",
+    [string]$Generator,
+    [string]$InstallDir,  
+    [string]$SymbolDir,  
+    [bool]$Force = $false,        # 是否强制重新构建
+    [bool]$Cleanup = $true        # 是否在构建完成后删除源码和构建目录
+)
+
+# 目标文件
+$DllPath = "$InstallDir/bin/lerc.dll"
+
+# 依赖库数组
+$Librarys = @()  
+
+# 符号库文件
+$PdbFiles = @(
+    "RelWithDebInfo\Lerc.pdb"
+)
+
+# 额外构建参数
+$CMakeCacheVariables = @{}
+
+. ./build-common.ps1 -Name $Name `
+    -SourceDir $SourceDir `
+    -InstallDir $InstallDir `
+    -SymbolDir $SymbolDir `
+    -Generator $Generator `
+    -TargetDll $DllPath `
+    -PdbFiles $PdbFiles `
+    -CMakeCacheVariables $CMakeCacheVariables `
+    -MultiConfig $true `
+    -Force $Force `
+    -Cleanup $Cleanup `
+    -Librarys $Librarys
